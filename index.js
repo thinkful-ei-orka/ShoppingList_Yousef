260fd26fd68264995ac4ead1e5080c78f28aec54
yghaddar <yaghaddar@outlook.com>
2020-04-23 00:29:20 -0400

    updated functions

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 function handleAddItem () {
   $('#js-shopping-list-form').on('submit', function (event) {
     event.preventDefault()
-    const newItem = $(event.currentarget).find('.js-shopping-list-entry').val()
+    const addItem = $('input').val()
     $('.shopping-list').append(` <li>
-          <span class="shopping-item">${newItem}</span>
+          <span class="shopping-item">${addItem}</span>
           <div class="shopping-item-controls">
             <button class="shopping-item-toggle">
               <span class="button-label">check</span>
@@ -18,25 +18,18 @@ function handleAddItem () {
   })
 }
 
-
-function checkAndUncheck() {
-$('.shopping-item-toggle').on('click', function (event) {
+function checkAndUncheck () {
+  $('.shopping-item-toggle').on('click', '.shopping-item-toggle', function (event) {
     console.log('this is running')
-    event.preventDefault()
-    $('.shopping-list').on('click', '.shopping-item-toggle').toggleClass('shopping-item__checked')
-})
+    this.closest('li').toggleClass('shopping-item__checked')
+  })
 }
 
-function removeItem() {
-$('.shopping-item-delete').on('click', function (event){
-    event.preventDefault()
-
-})
+function removeItem () {
+  $('.shopping-list').on('click', '.shopping-item-delete', function (event) {
+    this.closest('li').remove()
+  })
 }
-
-// $('.shopping-item-toggle').on('click', function(event){
-//     event.currentarget().toggleClass('shopping-item__checked')
-// })
 
 $(handleAddItem)
 $(checkAndUncheck)
